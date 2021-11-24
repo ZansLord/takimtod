a7d63457f6f55d5d1a293624e1b0af0924ba6984
ZansLord <shikaizaidan@gmail.com>
2021-11-24 08:41:41 +0700

    first commit

---
+++ message/donasi.js
@@ -0,0 +1,15 @@
+exports.dodo = (prefix) => {
+return`
+╭────────❲ *DONASI* ❳
+│
+│  Bantu Yah agar botnya on terus,
+│  dengan donasi seiklasnya 🙂
+│
+│  Dana : 082194424412 
+│  Pulsa : 083140891091
+│ 
+│ Makasih yang udah donasi
+│ ( ◜‿◝ )
+│
+╰────────⏣`
+}
